f7339e7acb4e54596a8b12fc846e461a6e958f01
aquillae7 <aquillae777@gmail.com>
2023-11-04 23:20:46 -0300

    up

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,20 +1,10 @@
 'use client'
-import react from 'react';
+import react from "react";
+
+
 export const Home = () =>
  {
-  return 
-  ( 
-  <> 
-  <h1> hello wolrd </h1>
-  <h1> hello wolrd </h1>
-  <h1> hello wolrd </h1>
-  <h1> hello wolrd </h1>
-  <h1> hello wolrd </h1>
-  <h1> hello wolrd </h1>
-  <h1> hello wolrd </h1>
-  <h1> hello wolrd </h1>
-  <h1> hello wolrd </h1>
-  </>
-  )
+  return(<> 
+  <h1> ola </h1></>) 
 }
 export default Home;
