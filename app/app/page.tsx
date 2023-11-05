57e4edeb0bbce3418e6f9842dcb103c24cded2b1
Arthur-Sabbadini <arthurflash36@gmail.com>
2023-11-04 23:15:27 -0300

    configuração de ambiente

---
+++ app/app/page.tsx
@@ -0,0 +1,20 @@
+'use client'
+import react from 'react';
+export const Home = () =>
+ {
+  return 
+  ( 
+  <> 
+  <h1> hello wolrd </h1>
+  <h1> hello wolrd </h1>
+  <h1> hello wolrd </h1>
+  <h1> hello wolrd </h1>
+  <h1> hello wolrd </h1>
+  <h1> hello wolrd </h1>
+  <h1> hello wolrd </h1>
+  <h1> hello wolrd </h1>
+  <h1> hello wolrd </h1>
+  </>
+  )
+}
+export default Home;
